e45bc901f2e84ba157056b2a663634209c970289
Francisco Mat <francisco@f5sites.com>
2017-11-27 15:35:32 -0200

    hide collapse v1

--- page-focus.php
+++ page-focus.php
@@ -11,16 +11,30 @@
 	);
 	echo $user_id = bp_displayed_user_id();
 	echo $member_type = bp_get_current_member_type();
-	var_dump(bp_has_members(  ) );die;*/?>
-	<div class="col-xs-2 sidebar_inverted" id="default_sidebar">
-		<?php the_widget("BP_Core_Members_Widget", "Team's Members"); ?>
+	var_dump(bp_has_members(  ) );die;
+	onclick="javascript:colunsHeightAlwaysTheSame();"
+	*/?>
+
+	
+	
+	
+	<div id="default_sidebar" class="col-xs-2 sidebar_inverted">
+		<button data-toggle="collapse" data-target="#default_sidebar_in" class="collapse_button coll_left" ><span class="glyphicon glyphicon-resize-horizontal"></span></button>
+		<div id="default_sidebar_in" class="width collapse in">
+			<?php the_widget("BP_Core_Members_Widget", "Team's Members"); ?>
+		</div>
 	</div>
 
-	<div class="col-xs-5" id="content_column">
+	<div id="content_column" class="col-xs-5">
 		<?php do_action("projectimer_show_clock_simplist"); ?>
 		<?php do_action("projectimer_display_task_tabs"); ?>
 	</div>
 
-	<div class="col-xs-5" id="activity_sidebar">
-		<?php do_action("projectimer_display_recent_activities"); ?>
+	
+
+	<div id="activity_sidebar" class="col-xs-5 sidebar_inverted">
+		<button data-toggle="collapse" data-target="#activity_sidebar_in" class="collapse_button coll_right" ><span class="glyphicon glyphicon-resize-horizontal"></span></button>
+		<div id="activity_sidebar_in" class="width collapse in">
+			<?php do_action("projectimer_display_recent_activities"); ?>
+		</div>
 	</div>
