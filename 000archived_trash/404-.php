d8cdd1c6f1a4f880cc41daa2bc65ddf18dee701a
Francisco Mat <francisco@f5sites.com>
2018-04-30 19:25:37 -0300

    little improvements

--- 000archived_trash/404-.php
+++
@@ -1,9 +0,0 @@
-<?php
-$domainf = $_SERVER["REQUEST_URI"];
-$domainf = explode('/', $domainf);
-$page = $domainf[count($domainf)-2];
-
-if($page=="focus") {
-	require_once("t-focar.php");
-}
-
--- 000archived_trash/single-blog_post.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-	<div id="content" class="content_nosidebar">
-		<div class="padder">
-a
-		</div><!-- .padder -->
-	</div><!-- #content -->
-
-<?php get_footer(); ?>
